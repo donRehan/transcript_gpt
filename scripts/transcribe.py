0f3556ec42087f40b0e45bf095067128ad080948
BluBird4 <alhussienrehan@hotmail.com>
2023-05-02 19:59:13 +0200

    Final

--- scripts/transcribe.py
+++ scripts/transcribe.py
@@ -5,5 +5,12 @@ import openai
 
 openai_api_key = os.environ.get('OPENAI_API_KEY')
 video_url = sys.argv[1]
-audio_file = os.path.join(os.getcwd(), 'tmp', video_url + '.m4a')
-print(audio_file)
+audio_file_path = os.path.join(os.getcwd(), 'tmp', video_url + '.m4a')
+
+audio_file = open(audio_file_path, 'rb')
+transcript = openai.Audio.transcribe(
+    file=audio_file,
+    model="whisper-1",
+    response_format="srt",
+    prompt="I am alhussien Rehan and this is an application for the OpenAI api practice for skills. I am going to transcribe this audio file.",
+)
